13374a97852f7d93a37affe54067fed8ea377492
Zoe Kendall <zkendall@zkendall.mtv.corp.google.com>
2018-10-04 11:30:12 -0700

    Cloned hideIt.sh from tadly/hideIt.sh and updated Polybar to auto-hide until mouseover

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,7 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar on all monitors
+# Launch and auto-hide polybar on all monitors
+WIDTH=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+' | cut -d"x" -f1)
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload example &
+  MONITOR=$m polybar --reload tray &
+  sleep 2;
+  sh ~/dotfiles/polybar/.config/polybar/hideIt.sh --name "^polybar-tray_"$m"$" --direction top --region 0x0+$WIDTH+10 &
 done
