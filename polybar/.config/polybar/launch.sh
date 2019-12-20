76b4c8020acfa0b843cf91028cec88169084dc45
Zoe Kendall <zkendall@google.com>
2019-12-20 09:36:56 -0800

    Configure a background bar that watches the weather

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-HOST=$(hostname)
-
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch and auto-hide polybar on all monitors
+# Launch normal polybar on all monitors, optionally hiding them
 WIDTH=$(xdpyinfo | grep -oP 'dimensions:\s+\K\S+' | cut -d"x" -f1)
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   # Check for a battery power supply as indicator for displaying the expanded
@@ -21,3 +19,14 @@ for m in $(polybar --list-monitors | cut -d":" -f1); do
   # sleep 5;
   # sh ~/dotfiles/polybar/.config/polybar/hideIt.sh --name "^polybar-tray_"$m"$" --direction top --region 0x0+$WIDTH+10 &
 done
+
+# Start and hide an ipc weather watcher; based on
+# https://github.com/ntcarlson/dotfiles/blob/gamma/i3/scripts/polybar_wrapper,
+# linked from
+# reddit.com/r/unixporn/comments/92guq6/i3polybar_animated_polybar_drop_down_menus/
+MONITOR="DP-5" polybar weather &
+BAR_ID=$!
+echo "weather watcher id: $BAR_ID"
+ln -sf /tmp/polybar_mqueue.$BAR_ID /tmp/polybar_mqueue_weather
+sleep 2
+echo "cmd:hide" >> /tmp/polybar_mqueue_weather
