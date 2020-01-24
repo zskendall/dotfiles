9b7357b2aa42b2ed85208690d1f74027a55e1f9f
Zoe Kendall <zkendall@google.com>
2020-01-24 10:01:01 -0800

    move meta as i3 mod

--- qmk/corne/keymap.c
+++ qmk/corne/keymap.c
@@ -5,6 +5,7 @@ extern keymap_config_t keymap_config;
 
 enum user_layers {
   _QWERTY,
+  _COLEMAK,
   _NUM,
   _NAV,
   _MOUSE,
@@ -17,12 +18,14 @@ enum user_layers {
 #define MS_TOG TG(_MOUSE)
 #define KC_UNDS LSFT(KC_MINS)
 #define VTERM LCTL(S(KC_F1))
+#define CMAK TG(_COLEMAK)
+#define TERM LGUI(KC_ENT)
 
 /** Dynamic mod-tap keycode macros. */
 #define CTL_ESC LCTL_T(KC_ESC)
 #define CTL_QUOT RCTL_T(KC_QUOT)
 #define LALT_SPC LALT_T(KC_SPC)
-#define RALT_RET RALT_T(KC_ENT)
+#define META_RET RGUI_T(KC_ENT)
 
 /** Dynamic layer-tap keycode macros. */
 #define NUM_LB LT(_NUM, KC_LBRC)
@@ -39,35 +42,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * | Esc  |  A   |  S   |  D   |  F   |  G   |                 |   H  |   J  |   K  |   L  |   ;  |   '  |
      * | LCtrl|      |      |      |      |      |                 |      |      |      |      |      | LCtrl|
      * ├──────┼──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┼──────┤
-     * | Meta |  Z   |  X   |  C   |  V   |  B   |                 |   N  |   M  |   ,  |   .  |   /  |   -  |
+     * | CMAK |  Z   |  X   |  C   |  V   |  B   |                 |   N  |   M  |   ,  |   .  |   /  |   -  |
      * └──────┴──────┴──────┴──┬───┴──┬───┴──┬───┴──┐          ┌───┴──┬───┴──┬───┴──┬───┴──────┴──────┴──────┘
-     *                         |  (   |  [   | Spc  |          |  Ret |   ]  |   )  |
+     *                         |  (   | Spc  |  [   |          |   ]  |  Ret |   )  |
      *                         |LShft | LAlt |*Num* |          | *Nav*| RAlt | RShft|
      *                         └──────┴──────┴──────┘          └──────┴──────┴──────┘
      */
     [_QWERTY] = LAYOUT(
         KC_TAB , KC_Q, KC_W, KC_E, KC_R, KC_T,                        KC_Y, KC_U, KC_I   , KC_O  , KC_P   , KC_BSPC,
         CTL_ESC, KC_A, KC_S, KC_D, KC_F, KC_G,                        KC_H, KC_J, KC_K   , KC_L  , KC_SCLN, CTL_QUOT,
-        KC_LGUI, KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
-                        KC_LSPO, LALT_SPC, NUM_LB,                NAV_RB, RALT_RET, KC_RSPC
+        CMAK   , KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MINS,
+                        KC_LSPO, LALT_SPC, NUM_LB,                NAV_RB, META_RET, KC_RSPC
+    ),
+
+    /**
+     * Modified Colemak Layer
+     * ┌──────┬──────┬──────┬──────┬──────┬──────┐                 ┌──────┬──────┬──────┬──────┬──────┬──────┐
+     * |//////|  Q   |  W   |  F   |  P   |  G   |                 |   J  |   L  |   U  |   Y  |   ;  |//////|
+     * ├──────┼──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┼──────┤
+     * |//////|  A   |  S   |  R   |  T   |  D   |                 |   H  |   N  |   E  |   I  |   O  |   '  |
+     * ├──────┼──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┼──────┤
+     * |//////|  Z   |  X   |  C   |  V   |  B   |                 |   K  |   M  |   ,  |   .  |   /  |   -  |
+     * └──────┴──────┴──────┴──┬───┴──┬───┴──┬───┴──┐          ┌───┴──┬───┴──┬───┴──┬───┴──────┴──────┴──────┘
+     *                         |//////|//////|//////|          |//////|//////|//////|
+     *                         └──────┴──────┴──────┘          └──────┴──────┴──────┘
+     */
+    [_COLEMAK] = LAYOUT(
+	_____, _____, _____, KC_F , KC_P , KC_G ,		      KC_J, KC_L , KC_U , KC_Y , KC_SCLN, _____,
+	_____, _____, _____, KC_R , KC_T , KC_D ,		      KC_H, KC_N , KC_E , KC_I , KC_O   , _____,
+	_____, _____, _____, _____, _____, _____,		      KC_K, _____, _____, _____, _____  , _____,
+                                  _____, _____, _____,            _____, _____, _____
     ),
 
     /**
      * Numeric Layer
      * ┌──────┬──────┬──────┬──────┬──────┬──────┐                 ┌──────┬──────┬──────┬──────┬──────┬──────┐
-     * |//////|      |      |//////|  `   |  _   |                 |   *  |   7  |   8  |   9  |   -  |  Del |
+     * |//////|      |      |  E   |  `   |  _   |                 |   *  |   7  |   8  |   9  |   -  |//////|
      * ├──────┼──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┼──────┤
-     * |//////|//////|      |//////|//////|  \   |                 |   /  |   4  |   5  |   6  |   +  |//////|
+     * |//////|//////|      |  D   |  F   |  \   |                 |   /  |   4  |   5  |   6  |   +  |//////|
      * ├──────┼──────┼──────┼──────┼──────┼──────┤		   ├──────┼──────┼──────┼──────┼──────┼──────┤
-     * |//////|      |      |//////|      |//////|                 |   =  |   1  |   2  |   3  |   .  |//////|
+     * | TERM |      |      |//////|      |//////|                 |   =  |   1  |   2  |   3  |   .  |  Del |
      * └──────┴──────┴──────┴──┬───┴──┬───┴──┬───┴──┐          ┌───┴──┬───┴──┬───┴──┬───┴──────┴──────┴──────┘
      *                         |//////|//////|*Pad* |          | 0    |//////|//////|
      *                         └──────┴──────┴──────┘          └──────┴──────┴──────┘
      */
     [_NUM] = LAYOUT(
-        _____, __x__, __x__, _____, KC_GRV, KC_UNDS,                    KC_PAST, KC_7 , KC_8 , KC_9  , KC_MINS, KC_DEL,
-        _____, _____, __x__, _____, _____ , KC_BSLS,                    KC_PSLS, KC_4 , KC_5 , KC_6  , KC_PPLS, _____  ,
-        _____, __x__, __x__, _____, __x__ , _____  ,                    KC_EQL , KC_1 , KC_2 , KC_3  , KC_DOT , _____  ,
+        _____, __x__, __x__, KC_E , KC_GRV, KC_UNDS,                    KC_PAST, KC_7 , KC_8 , KC_9  , KC_MINS, _____ ,
+        _____, _____, __x__, KC_D , KC_F  , KC_BSLS,                    KC_PSLS, KC_4 , KC_5 , KC_6  , KC_PPLS, _____ ,
+        TERM , __x__, _____, _____, __x__ , _____  ,                    KC_EQL , KC_1 , KC_2 , KC_3  , KC_DOT , KC_DEL,
                                   _____, _____, _____,              KC_0, _____, _____
     ),
 
@@ -76,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌──────┬──────┬──────┬──────┬──────┬──────┐                 ┌──────┬──────┬──────┬──────┬──────┬──────┐
      * |//////| ws1  | ws2  | ws3  | ws4  | ws5  |                 |  ws6 |  ws7 |  ws8 |  ws9 |  ws0 |//////|
      * ├──────┼──────┼──────┼──────┼──────┼──────┤                 ├──────┼──────┼──────┼──────┼──────┼──────┤
-     * |//////| Home | PgUp | PgDn | End  |      |                 |   ←  |   ↓  |   ↑  |  →   |      |//////|
+     * |//////| Home | PgUp | PgDn | End  |      |                 |   ←  |   ↓  |   ↑  |  →   |      | LOCK |
      * ├──────┼──────┼──────┼──────┼──────┼──────┤		   ├──────┼──────┼──────┼──────┼──────┼──────┤
      * |//////| tab1 | tab2 | tab3 | tab4 | tab5 |                 | tab6 | tab7 | tab8 | tabX |      |MS_TOG|
      * └──────┴──────┴──────┴──┬───┴──┬───┴──┬───┴──┐          ┌───┴──┬───┴──┬───┴──┬───┴──────┴──────┴──────┘
@@ -84,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                         └──────┴──────┴──────┘          └──────┴──────┴──────┘
      */
     [_NAV] = LAYOUT(
-        _____, RALT(KC_1), RALT(KC_2), RALT(KC_3), RALT(KC_4), RALT(KC_5),    RALT(KC_6), RALT(KC_7), RALT(KC_8), RALT(KC_9), RALT(KC_0),  _____,
-        _____, KC_HOME, KC_PGUP, KC_PGDN, KC_END, __x__,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, __x__, _____,
+        _____, RGUI(KC_1), RGUI(KC_2), RGUI(KC_3), RGUI(KC_4), RGUI(KC_5),    RGUI(KC_6), RGUI(KC_7), RGUI(KC_8), RGUI(KC_9), RGUI(KC_0),  _____,
+        _____, KC_HOME, KC_PGUP, KC_PGDN, KC_END, __x__,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, __x__, LGUI(KC_L),
         _____, LCTL(KC_1), LCTL(KC_2), LCTL(KC_3), LCTL(KC_4), LCTL(KC_5),    LCTL(KC_6), LCTL(KC_7), LCTL(KC_8), LCTL(KC_9), __x__,  MS_TOG,
                                         _____, _____, _____,              _____, _____, _____
     ),
